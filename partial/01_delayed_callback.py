9a89d538919ac994d4c055b859f82fc5998999fd
Emily Charles <emily.w.charles@gmail.com>
2023-06-18 21:15:40 -0400

    improve partials example

--- partial/01_delayed_callback.py
+++ partial/01_delayed_callback.py
@@ -1,23 +1,28 @@
 """
-In this exercise, you will write a function that calls another after a delay.
+In this exercise, you will write a function that returns after a delay.
 
-You should fill in the body of `call_after_delay` with two arguments:
-    - the first argument should be positional, and should be the current time (use `time.monotonic`)
-    - the second argument should be a keyword argument, and should be the time in seconds 
-        that the program should delay. Use `time.sleep` to implement the delay.
+First, update the value of `call_later` to be a call to `functools.partial`. Give the partial
+function one positional argument, which is the current time (use the `now` variable), and
+one keyword argument "delay". You can use the variable `delay_seconds` here.
 
-You should also fill in the value of `call_later` with a call to functools.partial, including the arguments
-as described above.
+You should also make two changes to the body of `report_timedelta`:
+    - update the argument to time.sleep
+    - update the return value to be the delta between the time the function was constructed
+        to the time it returned
+
+Lastly, fill in the value of `call_later` with a call to functools.partial that includes 
 
 When you have made the changes above, this script should complete with no assertion errors.
 """
-
 import functools
 import time
 
 
-def call_after_delay(*args, **kwargs):
-    pass
+def report_timedelta(*args, **kwargs):
+    time.sleep(0)  # Replace "0" here with the delay passed in to the partial function
+    return (
+        time.monotonic() - 0
+    )  # Replace "0" here with the value of "now" passed in to the partial function
 
 
 delay_seconds = 3
--- partial/01_delayed_callback_SOLUTION.py
+++ partial/01_delayed_callback_SOLUTION.py
@@ -2,13 +2,17 @@ import functools
 import time
 
 
-def get_time_delta(*args):
+def report_timedelta(*args, **kwargs):
+    time.sleep(kwargs.get("delay"))
     return time.monotonic() - args[0]
 
 
+delay_seconds = 3
 now = time.monotonic()
-call_later = functools.partial(get_time_delta, time.monotonic())
-time.sleep(1)
+
+call_later = functools.partial(
+    report_timedelta, now, delay=delay_seconds
+)  # Replace 'None' with a call to functools.partial
 time_delta = call_later()
 
-assert time_delta >= 1
+assert time_delta >= delay_seconds
